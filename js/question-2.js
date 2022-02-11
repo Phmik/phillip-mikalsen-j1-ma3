5e00ad5e66b13320cd83e5aa31429f099aa1feb3
Phmik <phm@live.no>
2022-02-11 11:56:56 +0100

    Finished Q2

--- js/question-2.js
+++ js/question-2.js
@@ -3,61 +3,36 @@
 
 const url = "https://api.rawg.io/api/games?dates=2019-01-01,2019-12-31&ordering=-rating&key=637d20fb82ae440f932a1c6ac2d0ee75"
 
+const resultsContainer = document.querySelector(".gameResults")
+const spinner = document.querySelector(".loader")
 async function getGames() {
-    const response = await fetch(url)
-    console.log(response);
-
-    const results = response.json()
-    console.log(results);
-
-    const games = results.all;
-    console.log(games);
-}
-
-getGames();
-
-/* const resultsContainer = document.querySelector(".gameResults");
-
-const spinner = document.querySelector(".loader");
-
-function createHTML(results) {
-    const games = results.all;
-
-    resultsContainer.innerHTML = "";
-
-    for(let i = 0; i < games.length; i++) {
         
-        if(i === 8) {
-            break;
-        }
-
-        resultsContainer.innerHTML += `<div class="result">
-    <ul>
-        <li>${games[i].name}</li>
-        <li>${games[i].rating}</li>
-        <li>${games[i].tags}</li>
-    </ul>
-    </div>`;
-    }
-
-    
-}
-
-async function getGames() {
-    
     try {
         const response = await fetch(url);
+       
+        const results = await response.json();
+       
+        const gamesResult = results.results;
+        resultsContainer.innerHTML = "";
 
-        const data = await response.json();
+        for(let i = 0; i < gamesResult.length; i++) {
+            console.log(gamesResult[i].name)
+            
+            if(i === 8) {
+                break;
+            }
         
-        const games = data.all;
-
-        createHTML(games);
-        
+            resultsContainer.innerHTML += `<div class="result">
+            <ul>
+                <li>Name: ${gamesResult[i].name}</li>
+                <li>Rating: ${gamesResult[i].rating}</li>
+                <li>Tags: ${gamesResult[i].tags.length}</li>
+            </ul>
+            </div>`;
+        }      
     } catch(error) {
         console.log(error);
     }
 }
 
-getGames(); */
-
+getGames();
